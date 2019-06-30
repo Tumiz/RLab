049768211de2327a63d9dbfc3e5c4ce1346ffbcb
huangjiajun <hh11698@163.com>
2019-06-30 23:38:53 +0800

    use new viz support

--- MountainCarContinuous/PG.py
+++ MountainCarContinuous/PG.py
@@ -1,18 +1,15 @@
 import gym
-from rlab import Model,normalsample,factors
+from rlab import Model,normalsample,Scope
 from torch import tensor
 from torch.optim import Adam
 from numpy import array,zeros,linalg
 from itertools import count
-import visdom
-env=gym.make("MountainCarContinuous-v0")
-viz=visdom.Visdom()
+
 class Agent():
     def __init__(self):
         self.m=Model(2,2)
         self.probs=[]
         self.score=0
-        self.peak=0
 
     def select_action(self,state):
         mu,sigma=self.m(tensor(state).float())
@@ -22,36 +19,25 @@ class Agent():
 
     def optimize(self):
         steps=len(self.probs)
-        loss=self.score/steps*-sum(self.probs)
-        # print(loss)
+        loss=100*self.score/steps*-sum(self.probs)
         self.m.optimize(loss)
         self.probs=[]
         return loss.item()
 
+env=gym.make("MountainCarContinuous-v0")
 agent=Agent()
-scores=[]
-viz.close(win=None)
-win=viz.line([0])
+scope=Scope(Scope.line)
 
 for j in count(1):
     state=env.reset()
     agent.score=state[0]
-    agent.peak=0
-    for t in range(1000):
+    for t in range(1,1000):
         action=agent.select_action(state)
         state, reward, done, _ = env.step(action)
         if agent.score<state[0]:
             agent.score=state[0]
-            agent.peak=t
-        # env.render()
         if done:
             break
     agent.optimize()
-    scores.append(agent.score)
-    if j%10==0:   
-        s=tensor(scores).mean().item()
-        scores.clear()
-        viz.line(X=[j],Y=[s],update="append",win=win)
+    scope.feed(agent.score,j,10)
     print("episode "+str(j)+" best "+str(agent.score)+" in "+str(t)+" steps")
-
-
